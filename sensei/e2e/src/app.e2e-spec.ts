ca61ae5fa0aef273728b528207e1e008fe22cef5
sunk0015 <sunk0015@umn.edu>
2020-12-30 21:18:11 -0600

    renamed to sensei

--- sensei/e2e/src/app.e2e-spec.ts
+++ sensei/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('sesei app is running!');
+    expect(await page.getTitleText()).toEqual('sensei app is running!');
   });
 
   afterEach(async () => {
--- sensei/src/app/app.component.spec.ts
+++ sensei/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sesei'`, () => {
+  it(`should have as title 'sensei'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sesei');
+    expect(app.title).toEqual('sensei');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('sesei app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('sensei app is running!');
   });
 });
